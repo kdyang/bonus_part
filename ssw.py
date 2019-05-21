e6493e6e51e28a4eeb4f9284ae7d3029413b299f
Deyang Kong <33001824+kdyang@users.noreply.github.com>
2019-05-21 22:55:50 +0800

    Add files via upload
    加入了特征图映射函数

--- ssw.py
+++ ssw.py
@@ -3,6 +3,7 @@ import selectivesearch
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
+import math
 def ssw(img,scale=500,sigma=0.9,min_size=10):
     img_lbl,regions=selectivesearch.selective_search(img,scale=scale,sigma=sigma,min_size=min_size)
     candidates =set()
@@ -46,3 +47,21 @@ def ssw(img,scale=500,sigma=0.9,min_size=10):
             #窗口过滤完之后的数量
     num_array=set(num_array)
     return num_array
+
+def feature_mapping(regions):
+    #如果保留pooling5，也就是映射到7*7
+    mapping=[]
+    for ele in regions:
+        mapping.append((math.floor(ele[0]/32)+1,math.floor(ele[1]/32)+1,max(math.ceil((ele[0]+ele[2])/32)-1-(math.floor(ele[0]/32)+1),0),
+        max(0,math.ceil((ele[1]+ele[3])/32)-1-(math.floor(ele[1]/32)+1))))
+    #如果不保留pooling5，也就是映射到14*14  
+#    for ele in regions:
+#        mapping.append((math.floor(ele[0]/16)+1,math.floor(ele[1]/16)+1,math.ceil((ele[0]+ele[2])/16)-1-(math.floor(ele[0]/16)+1),
+#        math.ceil((ele[1]+ele[3])/16)-1-(math.floor(ele[1]/16)+1)))     
+    return mapping
+
+img=cv2.imread('2011_002943.jpg')
+a=ssw(img)
+b=feature_mapping(a)
+print(a)
+print(b)
