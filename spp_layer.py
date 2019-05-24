ed511e6cef08841c6c4d1df6fefc75f981a6050f
Deyang Kong <33001824+kdyang@users.noreply.github.com>
2019-05-25 01:34:45 +0800

    Update spp_layer.py

--- spp_layer.py
+++ spp_layer.py
@@ -14,10 +14,10 @@ def spatial_pyramid_pool(previous_conv, num_sample, previous_conv_size, out_pool
     # print(previous_conv.size())
     for i in range(len(out_pool_size)):
         # print(previous_conv_size)
-        h_wid = int(math.ceil(previous_conv_size[0] / out_pool_size[i]))
-        w_wid = int(math.ceil(previous_conv_size[1] / out_pool_size[i]))
-        h_pad = int((h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2)
-        w_pad = int((w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2)
+        h_wid = math.ceil(previous_conv_size[0] / out_pool_size[i])
+        w_wid = math.ceil(previous_conv_size[1] / out_pool_size[i])
+        h_pad = ((h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2)
+        w_pad = ((w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2)
         maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
         x = maxpool(previous_conv)
         if(i == 0):
