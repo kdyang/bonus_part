75e3195efa1d999335beea79135c881c0a13e21b
Deyang Kong <33001824+kdyang@users.noreply.github.com>
2019-05-25 11:11:40 +0800

    Update data_pre.py

--- data_pre.py
+++ data_pre.py
@@ -18,6 +18,7 @@ class myDataSet(data.Dataset):
         self.root = root
         self.data_txt = open('annotations.txt', 'r')
         self.ssw_txt = open('ssw.txt', 'r')
+        self.ssw_test_txt = open('ssw_test.txt', 'r')
         self.istest = istest
         self.transform = transfrom
         self.imgs = []
@@ -31,18 +32,39 @@ class myDataSet(data.Dataset):
                         label_cur[int(words[i])] = 1
                         #label_cur.append(int(words[i]))
                     self.imgs.append([words[0], label_cur])
+                    
             else:
                 if not (words[0][0:4] == '2007' or words[0][0:4] == '2008'):
                     label_cur = [0 for i in range(20)]
                     for i in range(1, len(words)):
                         label_cur[int(words[i])] = 1
                         #label_cur.append(int(words[i]))
-                    self.imgs.append([words[0], label_cur])
+                    for linee in self.ssw_txt:
+                        linee = linee.rstrip()
+                        wordss = linee.split()
+                        ssw_block = torch.tensor([0,0,2,2])
+                        if words[0] == self.imgs[index][0]:
+                            ssw_block = torch.Tensor(floor((len(words) - 1) / 4), 4)
+                            for i in range(floor((len(words) - 1) / 4)):
+                                w=max(int(words[i * 4 + 3]),2)
+                                h=max(int(words[i*4+4]),2)
+                                ssw_block[i,0]=(30-w if (int(words[i*4+1])+w>=31) else int(words[i*4+1]))
+                                ssw_block[i,2]=w
+                                ssw_block[i,1]=(30-h if (int(words[i*4+2])+h>=31) else int(words[i*4+2]))
+                                ssw_block[i,3]=h                        
+                            break
+                        else:
+                            ssw_block = torch.tensor([[0,0,2,2]])
+
+                        
+                    self.imgs.append([words[0], ssw_block,label_cur])
                     
     def __getitem__(self, index):
         cur_img = Image.open(self.root + self.imgs[index][0] + '.jpg')
         data_once = self.transform(cur_img)
-        label_once = self.imgs[index][1]
+        label_once = self.imgs[index][2]
+        ssw_block=self.imgs[index][1]
+        '''
         for line in self.ssw_txt:
             line = line.rstrip()
             words = line.split()
@@ -58,6 +80,7 @@ class myDataSet(data.Dataset):
                     ssw_block[i,3]=h                    
                 
                 break
+                '''
         return data_once, ssw_block, torch.Tensor(label_once)
     
     def __len__(self):
